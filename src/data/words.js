780726d9c4a3178f59026ab9cfbbbcff3fa4aaab
ErickSBR0 <erick.sbr09@Gmail.com>
2026-07-13 20:28:05 -0300

    starting the project (comecando o projeto)

---
+++ src/data/words.js
@@ -0,0 +1,14 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Laranja", "Pera", "Uva"],
+  corpo: ["Braço", "Perna", "Coração", "Cérebro", "Olho"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa de Vídeo"],
+  programação: [
+    "Linguagem",
+    "Framework",
+    "Compilador",
+    "Depuração",
+    "Algoritmo",
+  ],
+  alimento: ["Arroz", "Feijão", "Macarrão", "Carne", "Frango"],
+};

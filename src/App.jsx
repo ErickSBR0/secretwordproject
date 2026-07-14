e33fc612aab8b93ed4ded8dc4e44b217ff0ba510
ErickSBR0 <erick.sbr09@Gmail.com>
2026-07-13 21:39:05 -0300

    adicionado estilo pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import StartScreen from "./components/StartScreen";
+import "./index.css";
 
 function App() {
-  return <h2>Secret Word</h2>;
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start-screen">
+      <h1>Secret Word</h1>
+      <p>Clique para começar!</p>
+      <button>Começar</button>
+    </div>
+  );
+};
+
+export default StartScreen;
